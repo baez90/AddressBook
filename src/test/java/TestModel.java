d18bf8710faec5c609f2152413b4873b611a51e4
baez <baez@k3-its.de>
2014-05-29 15:43:49 +0200

    Einfacher Unit-Test korrigiert

--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -2,16 +2,24 @@ import Model.Contact;
 import Model.ContactList;
 import junit.framework.TestCase;
 
+import java.util.logging.Logger;
+
 /**
  * Created by baez on 29.05.14.
  */
 public class TestModel extends TestCase {
+    private static Logger logger = Logger.getLogger(Contact.class.getName());
+    private Contact Heinz;
+    private Contact Ludwig;
+    private Contact Hans;
 
-    public void TestModel() {
-        Contact Heinz = new Contact("Karl", "Heinz", "heinz.hat@keineAdresse.de");
-        Contact Ludwig = new Contact("Ludwig", "Eder", "ludwig.eder@keineAdresse.de");
-        Contact Hans = new Contact("Hans", "Eicher", "hans.eicher@keineAdresse.de");
+    public void setUp() {
+        Heinz = new Contact("Karl", "Heinz", "heinz.hat@keineAdresse.de");
+        Ludwig = new Contact("Ludwig", "Eder", "ludwig.eder@keineAdresse.de");
+        Hans = new Contact("Hans", "Eicher", "hans.eicher@keineAdresse.de");
+    }
 
+    public void testModel() {
         ContactList contactList = new ContactList();
         contactList.add(Heinz);
         contactList.add(Ludwig);
@@ -21,7 +29,11 @@ public class TestModel extends TestCase {
         assertEquals(Ludwig.getName(), "Eder");
         assertEquals(Hans.getName(), "Eicher");
 
+        logger.info("Konstruktor erfolgreich getestet");
+
         Ludwig.setName("Kaiser");
         assertEquals(Ludwig.getName(), "Kaiser");
+
+        logger.info("Setter getestet");
     }
 }
