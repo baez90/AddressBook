ec293a0fee5732e5d0bfffa7c6e4c9bbec7ad106
baez <baez@k3-its.de>
2014-06-26 18:51:13 +0200

    Windows ist kacke :)

--- src/main/java/BusinessLogic/BlContacts.java
+++ src/main/java/BusinessLogic/BlContacts.java
@@ -3,6 +3,8 @@ package BusinessLogic;
 import Interfaces.*;
 import Model.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.time.ZoneId;
 import java.util.stream.Collectors;
@@ -51,7 +53,7 @@ public class BlContacts implements IBlContacts {
     @Override
     public boolean initDB() {
         if (!dbIsValid()) {
-            /*File databaseFile = new File(DbPath);
+            File databaseFile = new File(DbPath);
             try {
                 if (databaseFile.createNewFile()) {
                     IErrorLog.saveError("BlContacts", "Fehler beim Erstellen des Datenbankfiles", "");
@@ -61,7 +63,7 @@ public class BlContacts implements IBlContacts {
             } catch (IOException e) {
                 IErrorLog.saveError("BlContacts", "Fehler beim Erstellen des Datenbankfiles", e.toString());
                 return false;
-            }*/
+            }
             Statement stmt;
             Connection connection = prepareConnection();
             try {
